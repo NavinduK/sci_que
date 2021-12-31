f137fd313bdcf8cf43325ea29530bc6ecb9c4870
Navindu Kavishka <navindu.nk@gmail.com>
2021-12-31 22:13:04 +0530

    Answer change and interraction feature implementation

--- lib/Views/Question.dart
+++ lib/Views/Question.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:sci_que/Widgets/Answer.dart';
 
@@ -84,26 +82,26 @@ class _QuestionState extends State<Question> {
         child: Column(
           children: [
             SizedBox(
-              height: 25.0,
+              height: 15.0,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 if (_scoreTracker.length == 0)
                   SizedBox(
-                    height: 25.0,
+                    height: 24.0,
                   ),
                 if (_scoreTracker.length > 0) ..._scoreTracker
               ],
             ),
             SizedBox(
-              height: 25.0,
+              height: 15.0,
             ),
             Container(
               width: double.infinity,
               height: 130.0,
-              margin: EdgeInsets.only(bottom: 10.0, left: 30.0, right: 30.0),
-              padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+              margin: EdgeInsets.only(left: 30.0, right: 30.0),
+              padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
               decoration: BoxDecoration(
                 color: Colors.pinkAccent,
                 borderRadius: BorderRadius.circular(10.0),
@@ -121,13 +119,15 @@ class _QuestionState extends State<Question> {
               ),
             ),
             SizedBox(
-              height: 25.0,
+              height: 15.0,
             ),
             ...(_questions[_questionIndex]['answers']
                     as List<Map<String, Object>>)
                 .map(
               (answer) => Answer(
                 answerText: answer['answerText'].toString(),
+                correct: answer['score'] as bool,
+                answerWasSelected: answerWasSelected,
                 answerColor: answerWasSelected
                     ? answer['score'] as bool
                         ? Colors.green
@@ -198,7 +198,7 @@ class _QuestionState extends State<Question> {
               ),
             if (endOfQuiz)
               Container(
-                height: 100,
+                height: 80,
                 width: double.infinity,
                 color: Colors.black,
                 child: Center(
@@ -220,14 +220,91 @@ class _QuestionState extends State<Question> {
     );
   }
 }
+// final _questions = const [
+//   {
+//     'question': 'question',
+//     'answers': [
+//       {'answerText': 'answerText', 'score': true},
+//       {'answerText': 'answerText', 'score': false},
+//       {'answerText': 'answerText', 'score': false},
+//     ],
+//   },
+
+// ];
 
 final _questions = const [
   {
-    'question': 'question',
+    'question': 'How long is New Zealand’s Ninety Mile Beach?',
     'answers': [
-      {'answerText': 'answerText', 'score': true},
-      {'answerText': 'answerText', 'score': false},
-      {'answerText': 'answerText', 'score': false},
+      {'answerText': '88km, so 55 miles long.', 'score': true},
+      {'answerText': '55km, so 34 miles long.', 'score': false},
+      {'answerText': '90km, so 56 miles long.', 'score': false},
     ],
-  }
+  },
+  {
+    'question':
+        'In which month does the German festival of Oktoberfest mostly take place?',
+    'answers': [
+      {'answerText': 'January', 'score': false},
+      {'answerText': 'October', 'score': false},
+      {'answerText': 'September', 'score': true},
+    ],
+  },
+  {
+    'question': 'Who composed the music for Sonic the Hedgehog 3?',
+    'answers': [
+      {'answerText': 'Britney Spears', 'score': false},
+      {'answerText': 'Timbaland', 'score': false},
+      {'answerText': 'Michael Jackson', 'score': true},
+    ],
+  },
+  {
+    'question': 'In Georgia (the state), it’s illegal to eat what with a fork?',
+    'answers': [
+      {'answerText': 'Hamburgers', 'score': false},
+      {'answerText': 'Fried chicken', 'score': true},
+      {'answerText': 'Pizza', 'score': false},
+    ],
+  },
+  {
+    'question':
+        'Which part of his body did musician Gene Simmons from Kiss insure for one million dollars?',
+    'answers': [
+      {'answerText': 'His tongue', 'score': true},
+      {'answerText': 'His leg', 'score': false},
+      {'answerText': 'His butt', 'score': false},
+    ],
+  },
+  {
+    'question': 'In which country are Panama hats made?',
+    'answers': [
+      {'answerText': 'Ecuador', 'score': true},
+      {'answerText': 'Panama (duh)', 'score': false},
+      {'answerText': 'Portugal', 'score': false},
+    ],
+  },
+  {
+    'question': 'From which country do French fries originate?',
+    'answers': [
+      {'answerText': 'Belgium', 'score': true},
+      {'answerText': 'France (duh)', 'score': false},
+      {'answerText': 'Switzerland', 'score': false},
+    ],
+  },
+  {
+    'question': 'Which sea creature has three hearts?',
+    'answers': [
+      {'answerText': 'Great White Sharks', 'score': false},
+      {'answerText': 'Killer Whales', 'score': false},
+      {'answerText': 'The Octopus', 'score': true},
+    ],
+  },
+  {
+    'question': 'Which European country eats the most chocolate per capita?',
+    'answers': [
+      {'answerText': 'Belgium', 'score': false},
+      {'answerText': 'The Netherlands', 'score': false},
+      {'answerText': 'Switzerland', 'score': true},
+    ],
+  },
 ];
--- lib/Widgets/Answer.dart
+++ lib/Widgets/Answer.dart
@@ -4,11 +4,15 @@ class Answer extends StatelessWidget {
   const Answer({
     Key? key,
     required this.answerText,
+    required this.correct,
+    required this.answerWasSelected,
     required this.answerColor,
     required this.answerTap,
   }) : super(key: key);
 
   final String answerText;
+  final bool correct;
+  final bool answerWasSelected;
   final Color answerColor;
   final Function answerTap;
 
@@ -17,30 +21,46 @@ class Answer extends StatelessWidget {
     return InkWell(
       onTap: () => answerTap(),
       child: Container(
-          padding: EdgeInsets.all(15.0),
+          padding: EdgeInsets.all(16.0),
           margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 30.0),
           width: double.infinity,
           decoration: BoxDecoration(
             color: Colors.white,
-            border: Border.all(color: answerColor, width: 3),
+            // border: Border.all(color: answerColor, width: 3),
             borderRadius: BorderRadius.circular(10.0),
           ),
-          child: RichText(
-            text: TextSpan(
-              // style: Theme.of(context).textTheme.body1,
-              children: [
-                TextSpan(
-                    text: answerText, style: TextStyle(color: answerColor)),
-                WidgetSpan(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 2.0),
-                    child: Icon(
-                      Icons.check_circle,
-                      color: answerColor,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: RichText(
+                text: TextSpan(
+                  // style: Theme.of(context).textTheme.body1,
+                  children: [
+                    TextSpan(
+                        text: answerText,
+                        style: TextStyle(color: answerColor, fontSize: 16)),
+                    WidgetSpan(
+                      alignment: PlaceholderAlignment.middle,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                        child: answerWasSelected
+                            ? Icon(
+                                correct
+                                    ? Icons.check_circle
+                                    : Icons.cancel_sharp,
+                                color: answerColor,
+                              )
+                            : Icon(
+                                correct
+                                    ? Icons.check_circle
+                                    : Icons.cancel_sharp,
+                                color: Colors.white,
+                              ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
           )),
     );
